b98305b39d997857f220a71168dd0fe40f0a3393
Tony Khaov <tony.khaov@gmail.com>
2024-09-05 23:25:25 +0200

    clean up unecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
